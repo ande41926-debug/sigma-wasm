70625b140df3f890f32e7357867531d688f885cb
AndersJeffery <ande41926@auburnschl.edu>
2026-01-16 11:43:01 -0500

    fave food

--- src/routes/hello-wasm.ts
+++ src/routes/hello-wasm.ts
@@ -273,6 +273,8 @@ function validateHelloModule(exports: unknown): WasmModuleHello | null {
     increment_counter: wasmModuleExports.increment_counter,
     get_message: wasmModuleExports.get_message,
     set_message: wasmModuleExports.set_message,
+    get_fave_food: wasmModuleExports.get_message,
+    set_fave_food: wasmModuleExports.set_message,
   };
 }
 
